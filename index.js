d11c3b3e098e9246c562c54031af4ce06b92d171
warllison <warllison315bj@gmail.com>
2022-04-06 23:37:54 -0300

    Corrigindo erros

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express")
 const app = express()
 const port = 3000
 app.get("/", (req, res) => res.send("<h1>Deu Certo</h1>"))
-app.listen(port, () => console.info(`Servidor Rodando Na Porta ${port}`))
+app.listen(port, () => console.log(`Servidor Rodando Na Porta ${port}`))
