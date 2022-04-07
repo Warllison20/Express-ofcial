83512efeac62d73865f78cb7daaf1cf60029bf69
warllison <warllison315bj@gmail.com>
2022-04-06 23:29:45 -0300

    Corrigindo no Console

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => res.send("<h1>Deu Certo</h1>") )
+app.get("/", (req, res) => res.send("<h1>Deu Certo</h1>"))
 
-app.listen(port, () => console.infor(`Servidor Rodando Na Porta ${port}`))
+app.listen(port, () => console.info(`Servidor Rodando Na Porta ${port}`))
